a6c19388627f6f363e973f28e411930deab6696c
yvpan <vincentpan0314@gmail.com>
2021-01-22 09:49:26 -0600

    added package prerequisit

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+pip install --user matplotlib
+pip install --user scipy
+pip install --user scikit-learn
+pip install --user pandas
 pip install --user numpy==1.18.5
 pip install --user tensorflow==2.3.0
 pip install --user keras==2.4.3
